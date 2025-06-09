8b24b2b6222e0e4ee321bd2786a7e91ac5bafe44
Ezequiel Selis <ezequiel.selis@gmail.com>
2025-06-08 23:43:35 -0300

    Agrego Controller, service y repository para traer todos los aviones.

---
+++ src/main/java/com/airpatagonia/backend/Repositories/AvionRepository.java
@@ -0,0 +1,15 @@
+package com.airpatagonia.backend.Repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.airpatagonia.backend.models.Avion;
+
+@Repository
+public interface AvionRepository extends JpaRepository<Avion, Long> {
+
+    List<Avion> findAll();
+    
+}
