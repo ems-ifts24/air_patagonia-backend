69da47e761fce7828482efcc5eff8083268670e9
Ezequiel Selis <ezequiel.selis@gmail.com>
2025-06-10 22:59:53 -0300

    Actualizo Entity Pasajero y Vuelo

--- src/main/java/com/airpatagonia/backend/models/Pasajero.java
+++ src/main/java/com/airpatagonia/backend/models/Pasajero.java
@@ -1,8 +1,37 @@
 package com.airpatagonia.backend.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
 import lombok.Data;
 
+@Entity
+@Table(name = "Pasajero")
 @Data
 abstract class Pasajero {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "IdPasajero")
+    protected Long idPasajero;
+
+    @ManyToOne
+    @JoinColumn(name = "DetallePasaje")
     protected DetallePasaje detallePasaje;
+
+
+    public Pasajero getTipoPasajero() {
+        if (this instanceof PasajeroCliente) {
+            return (PasajeroCliente) this;
+        }
+        if (this instanceof PasajeroEmpleado) {
+            return (PasajeroEmpleado) this;
+        }
+        return null;
+    }
+
 }
--- src/main/java/com/airpatagonia/backend/models/Vuelo.java
+++ src/main/java/com/airpatagonia/backend/models/Vuelo.java
@@ -6,6 +6,18 @@ import com.airpatagonia.backend.models.Avion;
 import com.airpatagonia.backend.models.Empleado;
 import com.airpatagonia.backend.models.Pasajero;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.JoinTable;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -13,39 +25,44 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-// @Entity
+@Entity
+@Table(name = "Vuelo")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Vuelo {
 
-    // @Id
-    // @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "estado")
     private VueloEstado estado;
 
+    @Column(name = "fecha_partida")
     private LocalDateTime fechaPartida;
 
+    @Column(name = "fecha_arribo")
     private LocalDateTime fechaArribo;
 
-    // @ManyToOne
-    // @JoinColumn(name = "avion_id")
+    @ManyToOne
+    @JoinColumn(name = "avion_id")
     private Avion avion;
 
-    // @ManyToOne
-    // @JoinColumn(name = "origen_id")
+    @ManyToOne
+    @JoinColumn(name = "origen_id")
     private Aeropuerto origen;
 
-    // @ManyToOne
-    // @JoinColumn(name = "destino_id")
+    @ManyToOne
+    @JoinColumn(name = "destino_id")
     private Aeropuerto destino;
 
-    // @ManyToMany
-    // @JoinTable(name = "vuelo_tripulante", joinColumns = @JoinColumn(name = "vuelo_id"), inverseJoinColumns = @JoinColumn(name = "tripulante_id"))
+    @ManyToMany
+    @JoinTable(name = "vuelo_tripulante", joinColumns = @JoinColumn(name = "vuelo_id"), inverseJoinColumns = @JoinColumn(name = "tripulante_id"))
     private List<Empleado> tripulantes;
 
-    // @ManyToMany
-    // @JoinTable(name = "vuelo_pasajero", joinColumns = @JoinColumn(name = "vuelo_id"), inverseJoinColumns = @JoinColumn(name = "pasajero_id"))
+    @ManyToMany
+    @JoinTable(name = "vuelo_pasajero", joinColumns = @JoinColumn(name = "vuelo_id"), inverseJoinColumns = @JoinColumn(name = "pasajero_id"))
     private List<Pasajero> pasajeros;
 }
