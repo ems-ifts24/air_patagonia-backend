f40267584ecf4ce50e670b9932e76b24f973c502
Ezequiel Selis <ezequiel.selis@gmail.com>
2025-06-11 00:15:23 -0300

    Agrego Controller Repository y Service para Clientes y Empleados

---
+++ src/main/java/com/airpatagonia/backend/Services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.airpatagonia.backend.Services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.airpatagonia.backend.models.Cliente;
+import com.airpatagonia.backend.Repositories.ClienteRepository;
+
+@Service
+public class ClienteService {
+    
+    @Autowired
+    private ClienteRepository clienteRepository;
+    
+    public List<Cliente> getAllClientes() {
+        return clienteRepository.findAll();
+    }
+
+    public Cliente getClienteById(Long id) {
+        return clienteRepository.findById(id).orElse(null);
+    }
+}
