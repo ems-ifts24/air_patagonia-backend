7a5160a5a43d732ab78300fa0b88ee145deeff86
Ezequiel Selis <ezequiel.selis@gmail.com>
2025-06-14 21:03:25 -0300

    Agrego lógica para traer empleados por estado. Necesarios para listar y asignar a vuelos solo empleados DISPONIBLES.

--- src/main/java/com/airpatagonia/backend/Repositories/EmpleadoRepository.java
+++ src/main/java/com/airpatagonia/backend/Repositories/EmpleadoRepository.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.airpatagonia.backend.enums.EmpleadoEstado;
 import com.airpatagonia.backend.models.Empleado;
 
 @Repository
@@ -14,4 +15,6 @@ public interface EmpleadoRepository extends JpaRepository<Empleado, Long> {
     List<Empleado> findAll();
 
     Optional<Empleado> findById(Long id);
+
+    List<Empleado> findByEstadoEmpleado(EmpleadoEstado estado);
 }
