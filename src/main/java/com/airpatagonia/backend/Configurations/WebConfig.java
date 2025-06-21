d050a4900eb86fb28f60f00744de648cf9db6546
Ezequiel Selis <ezequiel.selis@gmail.com>
2025-06-21 17:37:39 -0300

    Cambio url de consumo en WebConfig (Cors) y cambio url de la base de datos para consumo interno.

--- src/main/java/com/airpatagonia/backend/Configurations/WebConfig.java
+++ src/main/java/com/airpatagonia/backend/Configurations/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-               .allowedOrigins("http://localhost:4200")
+               .allowedOrigins("https://ems-ifts24.github.io/air_patagonia-web/")
                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                .allowedHeaders("*")
                .allowCredentials(true);
